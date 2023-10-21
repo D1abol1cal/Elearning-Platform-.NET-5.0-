b02cce405224efba7b05e568643ebac6bd16279f
D1abol1cal <syednofeltalha2@gmail.com>
2023-10-21 20:37:54 +0500

    project creation

---
+++ IPT_PROJ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IPT_PROJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
